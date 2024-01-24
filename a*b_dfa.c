f5cc3881791e2f5e511ede072322aa4a03ee1a01
Ajay Dattu <dattuajay005@gmail.com>
2024-01-24 15:16:04 +0530

    Create a*b_dfa.c
    This code would accept the string whether it is in a*b

---
+++ a*b_dfa.c
@@ -0,0 +1,48 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+ char express[5]="a*b";
+  printf("  _______\n");
+  printf(" /       \\\n");
+  printf("|         |\n");
+  printf("|   O O   |\n");
+  printf("|    ∆    |\n");
+  printf(" \\_______/\n");
+ printf("it will accept expression is a*b");
+ int i=0;
+ int j=0;
+ int count=0;
+ int size;
+ printf("\n enter the size of the string");
+ scanf("%d",&size);
+ char test[size];
+ scanf("%s",test);
+ while(i<size)
+ {
+  if(test[j]=='a')
+  {
+   	count++;
+   	j++;
+  }	
+  else if(test[j]=='b')
+  {
+  	count++;
+  	j++;
+  	break;
+  }
+  i++;
+ }
+
+ if(count==size)
+ {
+  printf("\nstring accepted");
+ }
+ else
+ {
+ 	printf("\nnot accepted");
+ }
+
+    return 0;
+}
